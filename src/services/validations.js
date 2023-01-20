509519b06a4189f5956606116450a7d819d1032a
AndreiAvr123 <andrei.avram@student.vinci.be>
2023-01-20 10:18:32 +0100

    exported module in validations.js with RegExp added

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,4 +1,9 @@
 // TODO: Add here the function to validate
+module.exports = function (gamerTag) {
+    const reg = new RegExp("^[A-Z0-1$#'%*]{8}");
+
+    return reg.test(gamerTag);
+};
 
 const isEmpty = (label) => !label || label.length === 0;
 
